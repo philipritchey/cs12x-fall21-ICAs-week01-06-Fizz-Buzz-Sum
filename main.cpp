fa514150d85aa383d196bff72ebf312406fda71e
Philip Ritchey <philipritchey@users.noreply.github.com>
2021-08-30 21:35:49 +0000

    added i/o specification.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+/*
+input: integer n from standard input
+output: print to standard output the sum of the multiples of 3 or 5 but not both between 1 and n
+*/
+
 int main() {
   std::cout << "Hello World!\n";
 }
